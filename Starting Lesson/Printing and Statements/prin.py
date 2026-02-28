a809c4e9272d5244f7a17aa06689a615953ac142
chowdam89 <venturevideo45@gmail.com>
2026-02-28 18:18:46 +0530

    printt

---
+++ Starting Lesson/Printing and Statements/prin.py
@@ -0,0 +1,11 @@
+print("Hello, World!")
+print("Welcome to Python programming.")
+print("This is a simple print statement.")
+print("You can print numbers too:", 42)
+name = "Jayanth"
+print("You can also print variables:", name)
+
+print("You can use escape characters like \\n for a new line.")
+print("This is the first line.\nThis is the second line.")
+print(" You can also do opearations inside print statements: 5 + 3")
+print("\nThe result of 5 + 3 is:", 5 + 3)
