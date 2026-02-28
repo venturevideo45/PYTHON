7e7b02a0bd0a6f8895a634dc09ec1a9620f8a5a3
chowdam89 <venturevideo45@gmail.com>
2026-02-28 18:24:11 +0530

    keywordd

---
+++ Starting Lesson/World Of Programing/wor.py
@@ -0,0 +1,12 @@
+print("Welcome to the World of Programming!")
+print("In this world, you can create amazing things with code.")
+print("Let's start our journey together!")
+
+print("Firstly you can print numbers like: ", 42)
+
+print("you can also skip lines using \\n ")
+print("This is the first line.\nThis is the second line.")
+
+import keyword
+print("Here are some reserved keywords in Python:")
+print(keyword.kwlist)
