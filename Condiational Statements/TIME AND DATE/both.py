032ecb6901f29c8fe0db617f1d93d6c7c0d84651
chowdam89 <venturevideo45@gmail.com>
2026-04-04 18:04:55 +0530

    23

---
+++ Condiational Statements/TIME AND DATE/both.py
@@ -0,0 +1,6 @@
+import datetime
+current_time = datetime.datetime.now()
+print ("Time now at greenwich meridian is: ", end = "")
+print (current_time)
+import calendar
+print("\n", calendar.calendar (2021))
