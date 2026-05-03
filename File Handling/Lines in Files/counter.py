00f4dbe2bda9449385819bbb14db9bec091646af
chowdam89 <venturevideo45@gmail.com>
2026-05-03 18:10:04 +0530

    countt

---
+++ File Handling/Lines in Files/counter.py
@@ -0,0 +1,11 @@
+
+from collections import Counter
+file = open("Lines in Files\\counting.txt","r")
+Counter = 0
+Content = file.read()
+CoList = Content.split("\n")
+for i in CoList:
+   if i:
+       Counter += 1
+print("This is the number of lines in the file")
+print(Counter)
